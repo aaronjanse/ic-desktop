a56e027c5e1386d3fa9acbca8cdc02a0700bf18f
Aaron Janse <aaron.janse@gmail.com>
2017-12-24 15:04:32 -0800

    extend replacement ruleset

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -15,15 +15,30 @@ export function formatAsPercentage (num, leftpad = 7) {
 const courseNameWordPresets = {
   'ap': 'AP',
   'hon': 'Honors',
-  'h': 'Honors'
+  'h': 'Honors',
+  'pe': 'PE',
+  'course': ''
 }
 
 export function formatCourseName (str) {
-  return str.split(/\s(.+)/)[1].replace(/\w\S*/g, (txt) => {
+  str = str.toLowerCase()
+  str = str.replace(/(\d+)$/, '')
+  str = str.replace('span lang', 'spanish language')
+  str = str.replace('world his', 'world history')
+  str = str.replace('pre calc', 'pre calculus')
+  str = str.split(/\s(.+)/)[1].replace(/\w\S*/g, (txt) => {
     if (courseNameWordPresets.hasOwnProperty(txt.toLowerCase())) {
       return courseNameWordPresets[txt.toLowerCase()]
     } else {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
     }
   })
+
+  if (str.indexOf('Honors') !== -1) {
+    str = 'Honors ' + str.replace('Honors', '')
+  }
+
+  str = str.trim()
+
+  return str.replace(/(\d+)$/, '')
 }
