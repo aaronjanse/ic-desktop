fc51c189cd59e7f4ee5e6059d0f64d33e20495da
Jason Evans <jason@galaxa.co>
2017-12-23 20:31:42 -0800

    list assignments on grades page

--- src/renderer/components/Assignments.vue
+++ src/renderer/components/Assignments.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
     <div class="card" v-for="(section, key) in course.sections" v-bind:key="key">
-      <h4 class="card-header">{{ section.name }} <span class="badge badge-primary">{{ section.weight * 100 }}%</span></h4>
+      <h4 class="card-header">{{ section.name }}
+        <span class="badge badge-primary">{{ section.weight * 100 }}%</span>
+      </h4>
       <div class="card-body">
-        ...
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Course</th>
+              <th scope="col">Assignment</th>
+              <th scope="col">Score</th>
+              <th scope="col">Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(assignment, key) in section.assignments" v-bind:key="key">
+              <td>{{ course.name }}</td>
+              <td>{{ assignment.name }}</td>
+              <td>{{ assignment.ptsReceived }} / {{ assignment.ptsPossible }}</td>
+              <td>{{ Math.floor(assignment.ptsReceived / assignment.ptsPossible * 100) }}%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -25,7 +44,7 @@
   font-size: 0.8rem
 
 .card-body
-  padding: 1rem
+  padding: 0rem
 
 .card
   margin-bottom: 10px
