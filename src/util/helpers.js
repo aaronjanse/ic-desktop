cb89b74c78f2e53333742702cccc7e4fb0aa3eeb
Aaron Janse <aaron.janse@gmail.com>
2017-12-23 20:23:52 -0800

    add helper function to format course names

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,4 +1,4 @@
-const formatAsPercentage = (num, leftpad = 7) => {
+export function formatAsPercentage (num, leftpad = 7) {
   num = Math.round(num * 10000) / 100 + ''
   var value = Number(num)
   var res = num.split('.')
@@ -12,4 +12,19 @@ const formatAsPercentage = (num, leftpad = 7) => {
   return (num + '%').padStart(leftpad)
 }
 
-export default { formatAsPercentage }
+const courseNameWordPresets = {
+  'ap': 'AP',
+  'hon': 'Honors',
+  'h': 'Honors'
+}
+
+export function formatCourseName (str) {
+  return str.split(/\s(.+)/)[1].replace(/\w\S*/g, (txt) => {
+    console.log(txt)
+    if (courseNameWordPresets.hasOwnProperty(txt.toLowerCase())) {
+      return courseNameWordPresets[txt.toLowerCase()]
+    } else {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    }
+  })
+}
